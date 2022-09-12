75aa24737387d57afe8e945cc3eb09c89e856173
Asier García <asier98garcia@gmail.com>
2022-09-12 12:40:34 +0200

    Added a simple list of items with a pagination

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
 
 Route::get('/publicaciones', 'App\Http\Controllers\PublicacionesController@index');
